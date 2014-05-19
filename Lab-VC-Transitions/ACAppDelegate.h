52e0a4bea251df3b62b8ef3712a8a305e3dbd37a
Hari Karam Singh <harikaram@club15cc.com>
2014-05-19 12:34:25 +0100

    Initial Commit

---
+++ Lab-VC-Transitions/ACAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ACAppDelegate.h
+//  Lab-VC-Transitions
+//
+//  Created by Hari Karam Singh on 19/05/2014.
+//  Copyright (c) 2014 Air Craft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ACAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Lab-VC-Transitions/ACViewController.h
@@ -0,0 +1,13 @@
+//
+//  ACViewController.h
+//  Lab-VC-Transitions
+//
+//  Created by Hari Karam Singh on 19/05/2014.
+//  Copyright (c) 2014 Air Craft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ACViewController : UIViewController
+
+@end
